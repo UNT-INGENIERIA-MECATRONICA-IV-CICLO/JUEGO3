49d447a59d11ed04e4e8677d38e6ee5a6a5f02e6
hbobadilla27 <58265910+hbobadilla27@users.noreply.github.com>
2019-12-06 19:18:03 -0500

    Add files via upload
    AVANCE1

---
+++ src/gameObjetcs/Constantes.java
@@ -0,0 +1,33 @@
+
+package gameObjetcs;
+
+/**
+ *
+ * @author Intel
+ */
+public class Constantes {
+    //Ventana 
+    public static final int ancho=1100;
+    public static final int altura=680;
+    //Propiedades del jugador
+    public static final int velDispara=300;
+    public static final double rota=0.1;
+    public static final double ACC=0.2;
+    public static final double playermaxVel=7.0;
+    //Propiedades de laser
+    public static final double velLaser=15.0;
+    //Meteoro
+    public static final double MeteorVeloc=2;
+    public static final int MetScore=20;
+    //Enemigo
+    public static final int Nodo=160;
+    public static final double Masa=60;
+    public static final int VelocEnemigo=3;
+    
+    public static long enemfireRate=1000;
+    public static double EnemAnguloRango = Math.PI / 2;
+     
+    public static final int enemScore=40;
+    
+   
+}
